50c5df2adc0b5939dd213bba1bedc6442e69f334
avgduck <avgduck@gmail.com>
2025-11-10 02:25:14 -0500

    Remove unused static members

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -6,15 +6,8 @@ namespace NoStageEffects;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    public static Plugin Instance { get; private set; }
-    internal static ManualLogSource LogGlobal { get; private set; }
-
     private void Awake()
     {
-        Instance = this;
-        LogGlobal = base.Logger;
-        LogGlobal.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-        
         HarmonyPatches.PatchAll();
     }
 }

de75ae2d15ad7ab2299509ab74d0763e64f019e3
avgduck <avgduck@gmail.com>
2025-11-10 02:16:05 -0500

    Move StagePatches to a nested class of HarmonyPatches

--- src/HarmonyPatches.cs
+++ src/HarmonyPatches.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using LLScreen;
 using StageBackground;
 
 namespace NoStageEffects;
@@ -37,6 +36,72 @@ public static class HarmonyPatches
         [HarmonyPatch(typeof(GameCamera), nameof(GameCamera.StartGlitchWave))]
         [HarmonyPrefix]
         private static bool StartGlitchWave_Prefix()
+        {
+            return false;
+        }
+    }
+    
+    public static class StagePatches
+    {
+        [HarmonyPatch(typeof(ElevatorScript), nameof(ElevatorScript.SetState))]
+        [HarmonyPrefix]
+        private static void Elevator_SetState_Prefix(ref ElevatorScript.ElevatorState setState)
+        {
+            setState = ElevatorScript.ElevatorState.STOPPED;
+        }
+
+        private static bool SubwayIntro = false;
+        [HarmonyPatch(typeof(TrainScript), nameof(TrainScript.Start))]
+        [HarmonyPostfix]
+        private static void Subway_Start_Postfix(TrainScript __instance)
+        {
+            SubwayIntro = false;
+            __instance.SetState(TrainScript.TrainAnimationType.TRAIN_STATION_BIG_OUT);
+            SubwayIntro = true;
+        }
+        [HarmonyPatch(typeof(TrainScript), nameof(TrainScript.SetState))]
+        [HarmonyPrefix]
+        private static void Subway_SetState_Prefix(ref TrainScript.TrainAnimationType state)
+        {
+            if (!SubwayIntro) return;
+            
+            state = TrainScript.TrainAnimationType.TRAIN_STRAIGHT;
+        }
+        
+        [HarmonyPatch(typeof(DroneScript), nameof(DroneScript.StartDroneSequence))]
+        [HarmonyPrefix]
+        private static bool Streets_StartDroneSequence_Prefix()
+        {
+            return false;
+        }
+        
+        [HarmonyPatch(typeof(BlimpScript), nameof(BlimpScript.Update))]
+        [HarmonyPrefix]
+        private static bool Pool_Update_Prefix()
+        {
+            return false;
+        }
+        
+        [HarmonyPatch(typeof(AssemblyScript), nameof(AssemblyScript.Start))]
+        [HarmonyPostfix]
+        private static void Factory_Start_Postfix(AssemblyScript __instance)
+        {
+            __instance.animbucket1["ironBucketTrack"].enabled = false;
+            __instance.animbucket2["ironBucketTrack"].enabled = false;
+            __instance.animbucket3["ironBucketTrack"].enabled = false;
+            __instance.animbucket4["ironBucketTrack"].enabled = false;
+        }
+        
+        [HarmonyPatch(typeof(Stadium_ScreenCamController), nameof(Stadium_ScreenCamController.Awake))]
+        [HarmonyPostfix]
+        private static void Stadium_Awake_Postfix(Stadium_ScreenCamController __instance)
+        {
+            __instance._cam.enabled = false;
+        }
+        
+        [HarmonyPatch(typeof(SubmarineScript), nameof(SubmarineScript.StartAnim))]
+        [HarmonyPrefix]
+        private static bool Sewers_StartAnim_Prefix()
         {
             return false;
         }
